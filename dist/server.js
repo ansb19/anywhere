74a12e4f0ad1b60338f214d6ca61f21ca708745a
ansb19 <dhkdwk1997@naver.com>
2024-11-12 12:42:28 +0900

    CI/CD 23

--- dist/server.js
+++ dist/server.js
@@ -8,7 +8,7 @@ const dotenv_1 = __importDefault(require("dotenv"));
 const cors_1 = __importDefault(require("cors"));
 dotenv_1.default.config(); // dotenv 설정값 불러오는 함수
 const app = (0, express_1.default)(); // express 프레임워크 설정
-const port = process.env.PORT; // env 포트 설정
+const port = parseInt(process.env.PORT || "3000", 10); // env 포트 설정
 const cors_option = {
     origin: process.env.FRONT_END_API,
     // 통신형식 default 값 사용( 전부 허용)
@@ -18,6 +18,6 @@ const cors_option = {
 app.use((0, cors_1.default)());
 app.use(express_1.default.json()); // json 요청 본문을 파싱
 //app.use('/router,' mapRouter); rest api 라우터 설정
-app.listen(port, () => {
+app.listen(port, '0.0.0.0', () => {
     console.log(`서버 구동 중.. port:${port}`);
 });
