dc96c4863ffb12cbe9d045e754d40cecaf8cb55a
ansb19 <dhkdwk1997@naver.com>
2024-11-21 13:57:17 +0900

    api test1

--- src/routes/UserRoutes.ts
+++ src/routes/UserRoutes.ts
@@ -4,10 +4,16 @@ import UserController from '../controllers/UserController';
 const router = Router();
 
 // 사용자가 처음이면 회원가입 기존에 있으면 로그인
-router.post('/sign', UserController.createUser);
+router.get('/sign', UserController.createUser);
 
 // 닉네임을 변경
-router.get('/update/nickname', UserController.updateUser);
+router.put('/update/nickname', UserController.updateUser);
+
+router.get('find/nickname',UserController.getUserByNickname);
+
+router.delete('delete/nickname',UserController.deleteUser);
+
+
 
 // 유저의 등록한 장소 조회
 router.get('/place/find',);
