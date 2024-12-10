20a37d7cfc58a4181c1a3a8380943c8f9184270e
ansb19 <dhkdwk1997@naver.com>
2024-12-10 17:41:10 +0900

    redis docker-compose 11

--- src/data_source.ts
+++ src/data_source.ts
@@ -8,11 +8,12 @@ const db_host: string = process.env.DB_HOST_NAME || "localhost";
 const db_user_name: string = process.env.DB_USER_NAME || "my_username";
 const db_password: string = process.env.DB_PASSWORD || "my_password";
 const db_database: string = process.env.DB_DATABASE || "my_database";
+const db_port: string = process.env.DB_PORT || "my_database";
 
 export const AppDataSource = new DataSource({
   type: "postgres",
   host: db_host,
-  port: 5432,
+  port: parseInt(db_port),
   username: db_user_name,
   password: db_password,
   database: db_database,
